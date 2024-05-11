59534fade40f1a2cc93466bbc5c7542c3788db81
SJH-00 <tlstkd437@gmail.com>
2024-05-11 09:10:30 +0900

    initial commit

---
+++ src/main/java/umc/study/domain/mapping/MemberAgree.java
@@ -0,0 +1,26 @@
+package umc.study.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.Member;
+import umc.study.domain.Terms;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberAgree {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id")
+    private Terms terms;
+}
